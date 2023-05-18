eb9c8dfc3afdae2b222aa36e825f3c2a8a9ae9e7
Artur Saginbaev <decsterce@gmail.com>
2023-05-18 16:43:38 +0500

    Deleted hash structure
    Using hash node structure in this project not necessary. Only hash
    function stayed

--- lib/hash_item.h
+++ lib/hash_item.h
@@ -1,16 +1,8 @@
 #ifndef HASHITEM_H
 #define HASHITEM_H
 //структура хеш элемента(хеш, строка)
-typedef struct hash_item hash_item;
-
-struct hash_item
-{
-    unsigned long long key;
-    char* value;
-};
+typedef unsigned long long hash;
 
 unsigned long hash_function(char* str);
-
-hash_item* create_item(unsigned long long key, char* value);
 
 #endif /*HASHITEM_H*/
--- src/hashItem.c
+++ src/hashItem.c
@@ -3,19 +3,9 @@
 #include <math.h>
 #include "../lib/hash_item.h"
 
-//функция создания структуры хеш элемент
-hash_item* create_item(unsigned long long key, char* value) {
-    hash_item* item = (hash_item*) malloc (sizeof(hash_item));//выделяем память для 
-    item->key = malloc (sizeof(unsigned long long));
-    item->value = (char*) malloc (strlen(value) + 1);
-    item->key=key;
-    strcpy(item->value, value);
-    return item;
-}
-
 //Хеш функция, алгоритм которой изменяет строку в уникальную последовательность 
 unsigned long hash_function(char* str) {
-    int P=79; //максимальное количество различных символов в строке.
+    int P=46; //максимальное количество различных символов в строке.
     unsigned long long sum=0;
     for(int i=0;i<sizeof(str);i++){
         int ch=(int) str[i];//преобразовываем символ ASCII в int 

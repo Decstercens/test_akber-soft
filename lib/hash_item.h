d02b96234333c29c438cec7080ea0977dcf8ef54
Artur Saginbaev <decsterce@gmail.com>
2023-05-22 00:03:03 +0500

    Changed hashfunction
    Hash function now is more efficcient, give more unique results and mor
    capacity

--- lib/hash_item.h
+++ lib/hash_item.h
@@ -1,8 +1,8 @@
 #ifndef HASHITEM_H
 #define HASHITEM_H
-//структура хеш элемента(хеш, строка)
+
 typedef unsigned long long hash;
 
-unsigned long hash_function(char* str,int* len);
+unsigned long long hashFunction(char* str,int* len);
 
 #endif /*HASHITEM_H*/
--- src/hashItem.c
+++ src/hashItem.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include "../lib/hash_item.h"
 
 //Хеш функция, алгоритм которой изменяет строку в уникальную последовательность 
-unsigned long hash_function(char* str,int* len){
-    int P=46; //максимальное количество различных символов в строке.
-    unsigned long long sum=0;
-    for(int i=0;i<*len;i++){
-        int ch=(int) str[i];//преобразовываем символ ASCII в int 
-        ch*=pow(P,i);// и умножаем на его количество уникальных сиволов в степени позиции символа в строке
-        sum+=ch;//прибавляем число к сумме
+unsigned long long hashFunction(char* str,int* len){
+    const int p = 63;
+    const int mod = 1000000007; //
+    unsigned long long hash = 0, p_pow = 1;
+    for (int i=0; i<*len; ++i)
+    {
+        hash += ((str[i] - 'A' + 1) * p_pow) % mod; 
+        p_pow = (p_pow * p) % mod;
     }
-    return sum;
+    return hash;
 }
